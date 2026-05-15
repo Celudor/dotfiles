abf837e09479f6ccb6060c12ee511ee53e0bb4ee
Lukasz Stasienko <lukasz.stasienko@gmail.com>
2026-05-15 18:50:03 +0200

    remove tpm plugin from tmux conf

---
+++ install_tmux_catppuccin.sh
@@ -0,0 +1,3 @@
+rm -rf ~/.config/tmux/plugins/catppuccin
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
