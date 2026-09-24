2bc7d409ff0074129b7a1fcab8fd6cbfa7fa8676
Łukasz Stasieńko <lukasz.stasienko1@bayer.com>
2026-09-24 09:17:18 +0200

    remove projects

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,20 +1,19 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-    }
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+			dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+		}
 
-    alpha.setup(dashboard.opts)
-  end,
+		alpha.setup(dashboard.opts)
+	end,
 }
--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,26 +1,34 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
-  {
-    -- Detect tabstop and shiftwidth automatically
-    "tpope/vim-sleuth",
-  },
-  {
-    -- Powerful Git integration for Vim
-    "tpope/vim-fugitive",
-  },
-  {
-    -- GitHub integration for vim-fugitive
-    "tpope/vim-rhubarb",
-  },
-  {
-    -- Hints keybinds
-    "folke/which-key.nvim",
-  },
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-  },
+	{
+		-- Detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+	},
+	{
+		-- Powerful Git integration for Vim
+		"tpope/vim-fugitive",
+	},
+	{
+		-- GitHub integration for vim-fugitive
+		"tpope/vim-rhubarb",
+	},
+	{
+		-- Hints keybinds
+		"folke/which-key.nvim",
+	},
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				background_colour = "#000000",
+			})
+		end,
+	},
 }
